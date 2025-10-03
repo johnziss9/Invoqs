42037b9a55f3ebbe60e918292b35858a39e8fd24
John Z. <johnziss9@gmail.com>
2025-10-03 16:13:14 +0100

    Welcome message now grabs and shows user's first name.

---
+++ Models/UserInfo.cs
@@ -0,0 +1,11 @@
+namespace Invoqs.Models
+{
+    public class UserInfo
+    {
+        public int Id { get; set; }
+        public string Email { get; set; } = "";
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public string FullName { get; set; } = "";
+    }
+}
