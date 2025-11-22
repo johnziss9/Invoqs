91437f2e8677669491f96a96ba70de6595ebefd1
John Z. <johnziss9@gmail.com>
2025-11-22 14:28:49 +0000

    Added seq for logging.

--- Program.cs
+++ Program.cs
@@ -1,48 +1,72 @@
 using Invoqs.Components;
 using Invoqs.Interfaces;
 using Invoqs.Services;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
-
-var apiBaseUrl = builder.Configuration["API_BASE_URL"] ?? "http://localhost:5126";
-builder.Services.AddHttpClient("InvoiceAPI", client =>
+try
 {
-    client.BaseAddress = new Uri($"{apiBaseUrl}/api/");
-    client.DefaultRequestHeaders.Add("Accept", "application/json");
-});
+    Log.Information("Starting Invoqs Blazor");
+    
+    var builder = WebApplication.CreateBuilder(args);
 
-// Scoped HTTP client for dependency injection
-builder.Services.AddScoped(sp =>
-{
-    var clientFactory = sp.GetRequiredService<IHttpClientFactory>();
-    return clientFactory.CreateClient("InvoiceAPI");
-});
+    builder.Host.UseSerilog((context, services, configuration) => configuration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(services)
+        .Enrich.FromLogContext()
+        .Enrich.WithProperty("Application", "Invoqs.Blazor"));
 
-builder.Services.AddScoped<IAuthService, AuthService>();
-builder.Services.AddScoped<ICustomerService, CustomerService>();
-builder.Services.AddScoped<IJobService, JobService>();
-builder.Services.AddScoped<IInvoiceService, InvoiceService>();
-builder.Services.AddScoped<IReceiptService, ReceiptService>();
+    // Add services to the container.
+    builder.Services.AddRazorComponents()
+        .AddInteractiveServerComponents();
 
-var app = builder.Build();
+    var apiBaseUrl = builder.Configuration["API_BASE_URL"] ?? "http://localhost:5126";
+    builder.Services.AddHttpClient("InvoiceAPI", client =>
+    {
+        client.BaseAddress = new Uri($"{apiBaseUrl}/api/");
+        client.DefaultRequestHeaders.Add("Accept", "application/json");
+    });
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    // Scoped HTTP client for dependency injection
+    builder.Services.AddScoped(sp =>
+    {
+        var clientFactory = sp.GetRequiredService<IHttpClientFactory>();
+        return clientFactory.CreateClient("InvoiceAPI");
+    });
+
+    builder.Services.AddScoped<IAuthService, AuthService>();
+    builder.Services.AddScoped<ICustomerService, CustomerService>();
+    builder.Services.AddScoped<IJobService, JobService>();
+    builder.Services.AddScoped<IInvoiceService, InvoiceService>();
+    builder.Services.AddScoped<IReceiptService, ReceiptService>();
+
+    var app = builder.Build();
+
+    app.UseSerilogRequestLogging();
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Error", createScopeForErrors: true);
+        app.UseHsts();
+    }
+
+    app.UseStaticFiles();
+    app.UseAntiforgery();
+
+    app.MapRazorComponents<App>()
+        .AddInteractiveServerRenderMode();
+
+    app.Run();
 }
-
-// app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseAntiforgery();
-
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
